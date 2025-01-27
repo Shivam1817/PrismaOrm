1603dedfa03fdd5b1237fb74f955ffca558628ba
Shivam <shivam.chem21@iitg.ac.in>
2025-01-28 00:25:38 +0530

    added get todos and user details function

---
+++ src/getTodosAndUserDetails.ts
@@ -0,0 +1,21 @@
+import { PrismaClient } from "@prisma/client";
+import { get } from "http";
+
+const prisma = new PrismaClient();
+
+async function getTodosAndUserDetails(userId: number){
+    //using join to get todos and user details
+    const todos = await prisma.todo.findMany({
+        where:{
+            userId
+        },
+        select:{
+            user: true,
+            title: true,
+            description: true
+        }
+    })
+    console.log(todos);
+}
+
+getTodosAndUserDetails(1)
